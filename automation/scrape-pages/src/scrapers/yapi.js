fc07a1215b985bfb576472c55e9b45c8538764b8
FTH <canbekliodev@gmail.com>
2026-04-11 15:51:09 +0300

    Fix yapi scraper

--- automation/scrape-pages/src/scrapers/yapi.js
+++ automation/scrape-pages/src/scrapers/yapi.js
@@ -13,6 +13,17 @@ export const meta = {
     "https://www.yapikredi.com.tr/_ajaxproxy/general.aspx/LoadMainCurrencies"
 };
 
+const headers = {
+  "Accept": "text/plain, */*; q=0.01",
+  "Accept-Language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
+  "Cache-Control": "no-cache",
+  "Content-Type": "application/json; charset=UTF-8",
+  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36",
+  "X-Requested-With": "JQuery PageEvents"
+};
+
+const body = {}; // yeah, specifically no body but also json.
+
 function normalizeYapiRows(rows) {
   const normalized = {};
 
@@ -42,7 +53,9 @@ function extractProviderUpdatedAt(rows) {
 
 export async function scrape(ctx) {
   const response = await fetch(meta.sourceUrl, {
-    method: "GET",
+    method: "POST",
+    body: JSON.stringify(body),
+    headers,
     signal: ctx?.signal
   });
 
